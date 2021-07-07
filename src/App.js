63e6f345bfc1a513caaab223ef5af3370a1c3442
caarlosdamian <dapecarlos@gmail.com>
2021-07-06 22:13:29 -0500

    add some header and a navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from "react";
-
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
 const App = () => {
   return (
-    <div>
-      <h1>Hello word</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+
+                Photo Album
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </>
   );
 };
 
