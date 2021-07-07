76a3c886b988d19eb5800e60549e43028c979e4c
caarlosdamian <dapecarlos@gmail.com>
2021-07-06 22:33:35 -0500

    add custom hook as usestyles

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import {
   CardContent,
   CardMedia,
   CssBaseline,
+  Button,
   Grid,
   Toolbar,
   Container,
 } from "@material-ui/core";
 import { PhotoCamera } from "@material-ui/icons";
+import useStyles from "./Styles/styles";
 const App = () => {
+  const classes = useStyles();
   return (
     <>
       <CssBaseline />
@@ -23,7 +26,7 @@ const App = () => {
         </Toolbar>
       </AppBar>
       <main>
-        <div>
+        <div className={classes.container}>
           <Container maxWidth="sm">
             <Typography
               variant="h2"
@@ -31,9 +34,33 @@ const App = () => {
               color="textPrimary"
               gutterBottom
             >
-
-                Photo Album
+              Photo Album
             </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </Typography>
+            <div>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my Portafolio
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secundary Action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
           </Container>
         </div>
       </main>
